7120b1756b58fdf6f8bc061bfbff72217152108c
Wily Mayo <wilymayo@hotmail.com>
2021-03-30 13:36:19 -0400

    Add new script
    Able to now look around with mouse

---
+++ The Price Of Life/Assets/Script/CameraControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public int rotateSpeed = 100;
+
+
+    // Update is called once per frame
+    void Update()
+    {   // geting input of mosue x and if it is less then 0 
+        if(Input.GetAxis("Mouse X") < 0 )
+        {   // do this //
+            transform.Rotate(0, -rotateSpeed * Time.deltaTime, 0);
+        }
+        if(Input.GetAxis("Mouse X") > 0)
+        {
+            transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+        }
+    }
+}
