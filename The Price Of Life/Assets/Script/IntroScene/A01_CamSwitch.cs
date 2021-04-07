97e9f05e4514f2a27cf28f01c8b5466ad985f00b
Wily Mayo <wilymayo@hotmail.com>
2021-04-07 10:45:04 -0400

    Intro Scene
    Worked on adding more object to the scene and added  fadeIn screen and some UI panels for intro scene.

---
+++ The Price Of Life/Assets/Script/IntroScene/A01_CamSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class A01_CamSwitch : MonoBehaviour
+{
+    public GameObject camOne;
+    public GameObject camTwo;
+    public GameObject creditLeadDev;
+    public int secondsWaited = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(camSwitch());
+
+    }
+
+    IEnumerator camSwitch()
+    {
+        yield return new WaitForSeconds(secondsWaited);
+        creditLeadDev.SetActive(true);
+        yield return new WaitForSeconds(4);
+        camTwo.SetActive(true);
+        camOne.SetActive(false);
+    }
+
+}
