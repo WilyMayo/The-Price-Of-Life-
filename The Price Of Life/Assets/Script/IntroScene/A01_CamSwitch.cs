a9265d892d645a5a7f51422a99205d22752d4a5a
Wily Mayo <wilymayo@hotmail.com>
2021-11-28 23:08:03 -0500

    more Ambience to the the intro scene

--- The Price Of Life/Assets/Script/IntroScene/A01_CamSwitch.cs
+++ The Price Of Life/Assets/Script/IntroScene/A01_CamSwitch.cs
@@ -6,23 +6,29 @@ public class A01_CamSwitch : MonoBehaviour
 {
     public GameObject camOne;
     public GameObject camTwo;
+    public GameObject camThree;
     public GameObject creditLeadDev;
+    public GameObject creditStory;
     public int secondsWaited = 3;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(camSwitch());
+        StartCoroutine(CamSwitch());
 
     }
 
-    IEnumerator camSwitch()
+    IEnumerator CamSwitch()
     {
         yield return new WaitForSeconds(secondsWaited);
         creditLeadDev.SetActive(true);
         yield return new WaitForSeconds(4);
+        creditStory.SetActive(true);
         camTwo.SetActive(true);
         camOne.SetActive(false);
+        yield return new WaitForSeconds(4);
+        camThree.SetActive(true);
+        camTwo.SetActive(false);
     }
 
 }
